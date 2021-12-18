52748817f8fcef85752c9ddfc53d8df68666f267
BryantDev_ <Brayanmc.contacto@gmail.com>
2021-12-18 01:33:10 -0400

    Fixed animation of appearance/disappearance of the shopping box, also modified its size to match the screen

--- Events/shopBox-mobile.js
+++ Events/shopBox-mobile.js
@@ -1,19 +1,18 @@
 const shoppingBoxButton = document.getElementById('shopping-box-button');
 const shoppingBox = document.getElementById('selected-section');
 
+// Show the Shopping Box
 shoppingBoxButton.addEventListener('click', e => {
     shoppingBox.style.display = 'flex';
-    shoppingBox.style.animation = 'shoppingBoxAppear 0.2s ease-out';
+    shoppingBox.style.animation = 'shoppingBoxAppear 0.4s ease-out';
     
     shoppingBoxButton.style.display = 'none';
 });
 
-
+// Hide the Shopping Box
 const backButton = document.getElementById('close-button');
-
 backButton.addEventListener('click', e => {
-    shoppingBox.style.animation = 'shoppingBoxDisappear 0.2s ease-out';
-    shoppingBox.style.display = 'none';
-
+    shoppingBox.style.animation = 'shoppingBoxDisappear 0.4s ease-out';
+    setTimeout(() => shoppingBox.style.display = 'none', 300) //Wait for the animation to finish executing to hide it
     shoppingBoxButton.style.display = 'flex';
 })
